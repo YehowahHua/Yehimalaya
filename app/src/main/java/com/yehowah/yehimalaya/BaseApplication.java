93a5668bea957076c15f25270f550367563af6d5
YehowahHua <chen1124hua@foxmail.com>
2019-10-31 10:46:45 +0800

    获取喜马拉雅内容分类

--- app/src/main/java/com/yehowah/yehimalaya/BaseApplication.java
+++
@@ -1,6 +0,0 @@
-package com.yehowah.yehimalaya;
-
-import android.app.Application;
-
-public class BaseApplication extends Application {
-}
--- app/src/main/java/com/yehowah/yehimalaya/MainActivity.java
+++ app/src/main/java/com/yehowah/yehimalaya/MainActivity.java
@@ -1,14 +1,48 @@
 package com.yehowah.yehimalaya;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.ximalaya.ting.android.opensdk.datatrasfer.CommonRequest;
+import com.ximalaya.ting.android.opensdk.datatrasfer.IDataCallBack;
+import com.ximalaya.ting.android.opensdk.model.category.Category;
+import com.ximalaya.ting.android.opensdk.model.category.CategoryList;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
+    private static final String TAG = "MainActivity";
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        CommonRequest.getCategories(map, new IDataCallBack<CategoryList>() {
+            @Override
+            public void onSuccess(CategoryList categoryList) {
+                List<Category> categories = categoryList.getCategories();
+                if (categories != null){
+                    int size = categories.size();
+                    Log.i(TAG, " categories size--" + size);//31
+
+                    for (Category categore : categories){
+                        //获取种类
+                        Log.d(TAG, "categore --" + categore.getCategoryName());
+                    }
+                }
+            }
+
+            @Override
+            public void onError(int i, String s) {
+                Log.e(TAG, "onError: " + i + " error msg--" + s );//onError: 604 error msg--网络请求失败
+            }
+        });
+
     }
 }
